beee7a33a1819e097d6186c75afd791888a05d42
Scott Morken <smorken@nrcan.gc.ca>
2019-01-28 17:14:49 -0800

    adding example file, added main method

--- Program.cs
+++ Program.cs
@@ -1,20 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using CommandLine;
+using System.IO;
 namespace StandardImportToolPlugin
 {
     public class CommandLineOptions {
-
+        public string ConfigPath { get; set; }
     }
 
     class Program
     {
+        static string ReadFile(string path)
+        {
+            using (StreamReader reader = new StreamReader(File.OpenRead(path)))
+            {
+                return reader.ReadToEnd();
+            }
+        }
         static void Main(string[] args)
         {
-
+            var options = new CommandLineOptions();
+            var result = Parser.Default.ParseArguments<CommandLineOptions>(args);
+            result.WithParsed(a => {
+                string json = ReadFile(a.ConfigPath);
+                JsonConfigLoader jsonConfigLoader = new JsonConfigLoader();
+                Sitplugin sitplugin = jsonConfigLoader.Load(json);
+                sitplugin.Import();
+            });
         }
     }
 }
